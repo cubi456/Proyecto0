7240599466138dbf6c7e5d907d1a0d4b92c28699
isbarrei <isalvador_barreix@hotmail.com>
2015-09-03 23:18:40 -0300

    Debido a la baja de un compañero en el grupo se ha empleado cambios.

--- Simpsons/src/Gui/Gui.java
+++ Simpsons/src/Gui/Gui.java
@@ -6,23 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import java.awt.Color;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 
 public class Gui extends JFrame {
-	
-	private JPanel  panel;
-	private JLabel  text,image; 
-	private JButton button;
-	/**
-	 * Launch the application.
-	 */
+	private JTextArea textArea;
+	private JPanel contentPane;
+	private JLabel image;
+	private ImageIcon imageicon, imageicon2;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,42 +33,54 @@ public class Gui extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
+	/*	Creacion del frame
+	 * 
 	 */
 	public Gui() {
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 514, 404);
-		panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		setContentPane(panel);
+		setBounds(100, 100, 500, 500);
 		
+		//	Creacion de contenedor
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 		
-		text = new JLabel("Marge no voy a mentirte ..");
-		text.setVisible(false);
-		text.setFont(text.getFont().deriveFont(18.0f));
+		//	Panel Central
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
 		
-	    button = new JButton("Generar");
-		button.setBounds(0, 0, 200, 100);
+		//	Area de texto
+		textArea = new JTextArea();
+		panel.add(textArea, BorderLayout.SOUTH);
+		textArea.setText("Aprete el boton para obtener frase.");
+		
+		// Panel de control
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(30, 144, 255));
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		//	Creacion de boton 
+		JButton btnNewButton = new JButton("Iniciar");
+		panel_1.add(btnNewButton);
 		OyenteBoton oyente= new OyenteBoton();
-		button.addActionListener(oyente);
+		btnNewButton.addActionListener(oyente);
 		
-		ImageIcon imageicon = new ImageIcon(getClass().getResource("/Imagenes/imagen1.jpg"));
+		//	Creacion de labels
+		imageicon = new ImageIcon(getClass().getResource("/Imagenes/00.png"));
+		imageicon2 = new ImageIcon(getClass().getResource("/Imagenes/01.png"));
 		image = new JLabel();
 		image.setIcon(imageicon);
-		
-		
-		panel.add(text, BorderLayout.NORTH);
-		panel.add(image,BorderLayout.CENTER);
-		panel.add(button,BorderLayout.SOUTH);
-		
+		panel.add(image, BorderLayout.CENTER);
 		
 	}
 	
+		//Creacion e implementacion de Oyente
 	private class OyenteBoton implements ActionListener{
         public void actionPerformed(ActionEvent e){
-        	text.setVisible(true);
+        	image.setIcon(imageicon2);
+        	textArea.setText("Ayy esta grasa no se quita");
         }
 	}
 	
